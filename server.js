f73629dcf20c73ce92bb92d099916a8b45f7c0b5
Thomas Hibbard <thibbard@criticalmass.com>
2016-07-19 17:55:04 -0500

    adding angular/node promise exchange

--- server.js
+++ server.js
@@ -9,24 +9,22 @@ var Q = require('q');
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use('/dist', express.static(__dirname + '/dist'));
 
+
 app.get('/', function(req, res) {
 		res.sendfile('./index.html');
 });
 
-app.get('/test.js', function(req, res){
-
+app.get('/test', function(req, res){
 	var deferred = Q.defer();
-	fs.readFile('./gulpfile.js', 'utf-8', function (error, text) {
-			if (error) {
-				console.log('error', error);
-				deferred.reject(new Error(error));
-			} else {
-				res.send('resolved', text)
-				deferred.resolve(text);
-			}
+	fs.readFile("gulpfile.js", "utf-8", function (error, text) {
+		if (error) {
+			deferred.reject(new Error(error));
+		} else {
+			res.send(text);
+			deferred.resolve(text);
+		}
 	});
 	return deferred.promise;
-
 });
 
 app.listen(5000);
